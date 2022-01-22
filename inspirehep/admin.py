bb7d72377a9888eb65ec3f80e4cf61ea91bb8d57
Ebadi Javad (Systems Analysis) <j.ebadi@mebank.com>
2022-01-22 15:57:17 +0330

    Added country model

--- inspirehep/admin.py
+++ inspirehep/admin.py
@@ -1,3 +1,13 @@
+from atexit import register
+from operator import mod
 from django.contrib import admin
-
+from .models import (
+    Country,
+)
 # Register your models here.
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    model = Country
+    list_display = ('id', 'name', 'iso2', 'iso3')
+    search_fields = ('name', 'iso2', 'iso3') 
--- inspirehep/models.py
+++ inspirehep/models.py
@@ -1,3 +1,35 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    """
+    Django Model for Countries.
+
+    Attributes:
+        name (models.CharField): name of the country.
+        iso2 (models.CharField): two letters codes of the country.
+        iso3 (models.CharField): three letters codes of the country.
+    """
+
+    name = models.CharField(
+        max_length=64,
+    )
+
+    iso2 = models.CharField(
+        max_length=2,
+        unique=True,
+    )
+
+    iso3 = models.CharField(
+        max_length=3,
+        unique=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+    class Meta:
+        db_table = 'country'
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+        ordering = ('name',)
